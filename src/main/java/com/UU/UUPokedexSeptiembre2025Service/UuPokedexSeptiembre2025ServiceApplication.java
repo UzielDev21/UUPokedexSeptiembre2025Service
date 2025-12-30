f1b9446c3e0e38b213824d26c881667c150c371a
digis <digis@ALIEN27>
2025-12-30 10:56:50 -0600

    Inicio de proyecto PokeApki

---
+++ src/main/java/com/UU/UUPokedexSeptiembre2025Service/UuPokedexSeptiembre2025ServiceApplication.java
@@ -0,0 +1,13 @@
+package com.UU.UUPokedexSeptiembre2025Service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UuPokedexSeptiembre2025ServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UuPokedexSeptiembre2025ServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/UU/UUPokedexSeptiembre2025Service/UuPokedexSeptiembre2025ServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.UU.UUPokedexSeptiembre2025Service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UuPokedexSeptiembre2025ServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

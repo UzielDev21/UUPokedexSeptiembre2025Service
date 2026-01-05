12f2720283e2b7ca8616ceccd46bda105368ffbe
digis <digis@ALIEN27>
2026-01-05 17:18:51 -0600

    Test de Spring Security

---
+++ src/main/java/com/UU/UUPokedexSeptiembre2025Service/RestController/LoginRestController.java
@@ -0,0 +1,87 @@
+package com.UU.UUPokedexSeptiembre2025Service.RestController;
+
+import com.UU.UUPokedexSeptiembre2025Service.DAO.IUsuarioRepository;
+import com.UU.UUPokedexSeptiembre2025Service.JPA.Result;
+import com.UU.UUPokedexSeptiembre2025Service.JPA.UsuariosJPA;
+import com.UU.UUPokedexSeptiembre2025Service.Service.JwtService;
+import com.UU.UUPokedexSeptiembre2025Service.Service.TokenBlackListService;
+import java.util.Map;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api")
+public class LoginRestController {
+    
+    private final AuthenticationManager authenticationManager;
+    private final JwtService jwtService;
+    private final IUsuarioRepository iUsuarioRepository;
+    private final TokenBlackListService tokenBlackListService;
+    
+    public LoginRestController(
+            AuthenticationManager authenticationManager,
+            JwtService jwtService,
+            IUsuarioRepository iUsuarioRepository,
+            TokenBlackListService tokenBlackListService) throws Exception {
+        
+        this.authenticationManager = authenticationManager;
+        this.jwtService = jwtService;
+        this.iUsuarioRepository = iUsuarioRepository;
+        this.tokenBlackListService = tokenBlackListService;
+    }
+    
+    @PostMapping("Login")
+    public ResponseEntity Login(@RequestBody Map<String, String> json) {
+        
+        Result result = new Result();
+        
+        try {
+            
+            String userName = json.get("userName");
+            String password_Hash = json.get("password_Hash");
+            
+            UsernamePasswordAuthenticationToken auth = new UsernamePasswordAuthenticationToken(
+                    userName,
+                    password_Hash);
+            
+            try {
+                authenticationManager.authenticate(auth);
+            } catch (Exception ex) {
+                result.correct = false;
+                result.errorMessage = "Credenciales Invalidas";
+                result.status = 401;
+                return ResponseEntity.status(result.status).body(result);
+            }
+            
+            UsuariosJPA usuarioJPA = iUsuarioRepository.findByUserName(userName);
+            
+            if (usuarioJPA == null) {
+                result.correct = false;
+                result.errorMessage = "Usuario no encontrado";
+                result.status = 401;
+                return ResponseEntity.status(result.status).body(result);
+            }
+            
+            String rol = usuarioJPA.rolJPA.getRole_Name();
+            int user_Id = usuarioJPA.getUser_Id();
+            
+            String jwt = jwtService.GenerateTokenUser(userName, user_Id, rol);
+            result.correct = true;
+            result.status = 200;
+            result.object = jwt;
+            
+        } catch (Exception ex) {
+            result.correct = false;
+            result.errorMessage = ex.getLocalizedMessage();
+            result.ex = ex;
+            result.status = 500;
+        }
+        return ResponseEntity.status(result.status).body(result);
+    }
+    
+}
